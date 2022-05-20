1e19b543669d935978164422850c8dc6dad2c778
pubudumanoj <pubudumanoj@gmail.com>
2022-05-19 23:39:38 -0400

    removed work folder

--- work/c7/6cf3d5a681c7c044a3301cafe536cf/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-echo 
-mkdir temp
-mkdir merged
-inputs=`printf I= | sed 's\ \ I=\'`
-java -Xmx20G -Djava.io.tmpdir=temp/ -jar $PICARD_HOME/picard.jar MergeSamFiles $inputs OUTPUT=merged/Afraterculus.bam USE_THREADING=TRUE ASSUME_SORTED=TRUE VALIDATION_STRINGENCY=LENIENT
