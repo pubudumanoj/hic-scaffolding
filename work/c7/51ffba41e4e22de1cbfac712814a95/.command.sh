7bbfba17d734a0dbdfd84db232b8fb01ace3008d
pubudumanoj <pubudumanoj@gmail.com>
2022-05-25 23:41:55 -0400

    removed nextflow work folder added by mistakely

--- work/c7/51ffba41e4e22de1cbfac712814a95/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-mkdir temp
-mkdir merged
-inputs=`echo  | sed 's\ \ -I \g'`
-echo $inputs
-java -Xmx40g -Djava.io.tmpdir=temp/ -jar $PICARD_HOME/picard.jar MergeSamFiles -I $inputs -OUTPUT merged/Afraterculus.bam -USE_THREADING TRUE -ASSUME_SORTED TRUE -VALIDATION_STRINGENCY LENIENT
